4691347569d3b1a46a4fc4e165326bb35047940e
Karin Touchie <ktouchie@gmail.com>
2015-12-14 10:01:47 +0100

    Create an array and used .length

---
+++ Exercice3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Exercice3 {
+    
+    public static void main(String [] args) {  
+
+        int a[] = {2,5,3,1,4,0};
+        int num = a.length;
+                
+        System.out.println("The number of elements in the array {2,5,3,1,4,0} is " + num);
+    }
+}
